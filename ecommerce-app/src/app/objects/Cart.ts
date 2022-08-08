199b4e81e8a9ba543a1585e5f05eb8b3d5692278
Mike Kight <102483977+Mdkight@users.noreply.github.com>
2022-08-08 16:16:42 -0500

    built basic cart with add/remove item and checkout

--- ecommerce-app/src/app/objects/Cart.ts
+++ ecommerce-app/src/app/objects/Cart.ts
@@ -1,8 +1,11 @@
 import { Customer } from "./Customer";
+import { Transaction } from "./Transaction";
 
 export interface Cart{
     cartNumber:number;
     purchaseDate:Date;
     totalPrice:number;
     customer:Customer;
+    transactions:Transaction[];
+
 }
