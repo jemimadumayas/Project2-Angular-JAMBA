0ddda11f79fce35f29b5f08ea45439a86c0cac85
Jem D <jemimadumayas@gmail.com>
2022-08-10 12:26:07 -0700

    design

--- ecommerce-app/src/app/movie-list/movie-list.component.ts
+++ ecommerce-app/src/app/movie-list/movie-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from '../objects/Movie';
+import { RegisterService } from '../register.service';
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _GetService:RegisterService, private route:ActivatedRoute) { }
+  movies:Movie[] | any;
+  name:string | any;
 
   ngOnInit(): void {
+    this._GetService.getAllMovies().subscribe(data=>{ this.movies = data})
   }
 
+  addToCart(title:string/* ,customer:Customer */): void {
+      this.name=title;
+  /*   this._GetService.addItemToCart(customer, this.name) */
+    console.log("added to cart "+this.name);
+  }
 }
--- ecommerce-app/src/app/register.service.ts
+++ ecommerce-app/src/app/register.service.ts
@@ -2,13 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Customer } from './objects/Customer';
+import { Movie } from './objects/Movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  
+  baseurl = "http://localhost:8080/jamba";
+
 
   constructor(private _http:HttpClient) { }
+  
+  getAllMovies():Observable<Movie>{
+    return this._http.get<Movie>(this.baseurl+"/movie/all-movies")
+  }
   
   public authenticateUser(customer:Customer) : Observable<any>{
     return this._http.post<any>("http://localhost:8080/jamba/login",customer)
